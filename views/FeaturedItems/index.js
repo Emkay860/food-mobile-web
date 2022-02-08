a097ddb23a2e0483f282357132ece9f2e4722864
Majid Kareem <kareemmajid86@gmail.com>
2022-02-08 04:16:53 +0100

    Add smooth scroll animation to featuredItem slider

--- views/FeaturedItems/index.js
+++ views/FeaturedItems/index.js
@@ -7,13 +7,35 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import styles from './FeaturedItems.module.css';
 
 export default function FeaturedItems() {
+  const handleScroll = (prop) => (event) => {
+    const slider = document.getElementById('featuredContainer');
+    // smoothen slider animation
+    slider.style.scrollBehavior = 'smooth';
+
+    // Add delay to slide motion with setTimeout
+    if (prop === 'left') {
+      setTimeout(() => {
+        slider.scrollLeft -= 300;
+      }, 200);
+    } else if (prop === 'right') {
+      setTimeout(() => {
+        slider.scrollLeft += 300;
+      }, 200);
+    }
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <IconButton color="primary" aria-label="scroll left" component="span">
+        <IconButton
+          color="primary"
+          aria-label="scroll left"
+          component="span"
+          onClick={handleScroll('left')}
+        >
           <ChevronLeftIcon />
         </IconButton>
-        <div className={styles.featuredContainer}>
+        <div className={styles.featuredContainer} id="featuredContainer">
           <FeaturedItemCard />
           <FeaturedItemCard />
           <FeaturedItemCard />
@@ -28,6 +50,7 @@ export default function FeaturedItems() {
           aria-label="scroll right"
           component="span"
           size="large"
+          onClick={handleScroll('right')}
         >
           <ChevronRightIcon fontSize="inherit" />
         </IconButton>
