4eadca061766feaba9ad731dc88309ab330bf125
Majid Kareem <kareemmajid86@gmail.com>
2022-03-02 13:47:49 +0100

    Complete updatemenuItem dialog
    Add app theme to dialog

--- components/UpdateMenuItem/index.js
+++ components/UpdateMenuItem/index.js
@@ -5,23 +5,27 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import UpdateMenuForm from './UpdateMenuForm';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../../theme';
 
 export default function UpdateMenuItem({ scroll, open, handleClose }) {
   return (
-    <div>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll={scroll}
-        aria-labelledby="update-dialog-title"
-        aria-describedby="update-dialog-description"
-        maxWidth="xs"
-      >
-        <DialogTitle id="update-dialog-title">Update Item</DialogTitle>
-        <DialogContent dividers={scroll === 'paper'}>
-          <UpdateMenuForm />
-        </DialogContent>
-      </Dialog>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          scroll={scroll}
+          aria-labelledby="update-dialog-title"
+          aria-describedby="update-dialog-description"
+          maxWidth="xs"
+        >
+          <DialogTitle id="update-dialog-title">Update Item</DialogTitle>
+          <DialogContent dividers={scroll === 'paper'}>
+            <UpdateMenuForm />
+          </DialogContent>
+        </Dialog>
+      </ThemeProvider>
+    </>
   );
 }
